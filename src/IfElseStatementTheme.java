e7efdc58a763e6e6062ca94d9ee8f07f4fe7a4be
selbzt <nm263511@gmail.com>
2024-03-24 19:26:50 +0300

    Убрал пустую строку

--- src/IfElseStatementTheme.java
+++ src/IfElseStatementTheme.java
@@ -1,6 +1,5 @@
 public class IfElseStatementTheme {
     public static void main(String[] args) {
-
         System.out.println("1. Перевод псевдокода на язык Java");
 
         boolean isMale = true;
